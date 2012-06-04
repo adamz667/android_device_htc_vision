adbf49e02a125237f3da8d514b52c02501820e06
Jacek Adamski <adams@g.pl>
2012-06-04 12:54:26 +0200

    Add hot reboot
    Change-Id: Iebe810291d82f4582ea63a869fd856806973acca

--- cm.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, device/htc/vision/full_vision.mk)
-
-PRODUCT_RELEASE_NAME := Vision
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.0.4/IMM76I/87995:user/release-keys PRIVATE_BUILD_DESC="4.0.4 Andromadus release-keys"
-
-PRODUCT_NAME := Vision
-PRODUCT_DEVICE := vision
